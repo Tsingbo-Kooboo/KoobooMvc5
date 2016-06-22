9ad49aa1a4983b65f3fb1bf889111728a16673c0
qingbo <qingbo@kooboo.com>
2016-06-22 15:25:44 +0800

    update nuget package Kooboo.Core 4.5.0.622

--- Kooboo.CMS/CMSAssemblyInfoGlobal.cs
+++ Kooboo.CMS/CMSAssemblyInfoGlobal.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Yardi 2009-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("4.5.0.0612")]
-[assembly: AssemblyFileVersion("4.5.0.0612")]
+[assembly: AssemblyVersion("4.5.0.0622")]
+[assembly: AssemblyFileVersion("4.5.0.0622")]
