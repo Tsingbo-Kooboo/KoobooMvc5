f6e8827dd2fb57597be465ffcd44cccac426f1c6
rongzhi <rongzhi@kooboo.com>
2016-07-08 10:50:46 +0800

    Build 4.5.0.0708

--- Kooboo.CMS/CMSAssemblyInfoGlobal.cs
+++ Kooboo.CMS/CMSAssemblyInfoGlobal.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Yardi 2009-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("4.5.0.0701")]
-[assembly: AssemblyFileVersion("4.5.0.0701")]
+[assembly: AssemblyVersion("4.5.0.0708")]
+[assembly: AssemblyFileVersion("4.5.0.0708")]
