021dae863c56f9bee6366d1beff04b9b21ecaee7
Tsingbo-Kooboo <qingbo@kooboo.com>
2016-12-25 13:49:24 +0800

    code review

--- Kooboo.CMS/Publish/AccountProviders/Publish.bat
+++ Kooboo.CMS/Publish/AccountProviders/Publish.bat
@@ -1,6 +1,5 @@
-
 copy "..\..\Kooboo.CMS.Account.Persistence.EntityFramework\bin\Release\Kooboo.CMS.Account.Persistence.EntityFramework.dll" "EntityFramework\Kooboo.CMS.Account.Persistence.EntityFramework.dll"
 
-copy "..\..\..\Lib\EntityFramework\*.dll" "EntityFramework\*.*"
+copy "..\..\packages\EntityFramework.6.1.3\lib\net45\*.dll" "EntityFramework\*.*"
 
 ..\7z\7z a ..\Released\Account_Providers.zip EntityFramework\*.*
