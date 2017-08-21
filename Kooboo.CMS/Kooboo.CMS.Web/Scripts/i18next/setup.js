da6f2f581635dc63da02450a6982e4b9199912ba
hahqb <hahqb@qq.com>
2017-08-22 00:07:59 +0800

    i18next

---
+++ Kooboo.CMS/Kooboo.CMS.Web/Scripts/i18next/setup.js
@@ -0,0 +1,10 @@
+﻿jqueryI18next.init(i18nextInstance, $, {
+    tName: 't', // --> appends $.t = i18next.t 
+    i18nName: 'i18n', // --> appends $.i18n = i18next 
+    handleName: 'localize', // --> appends $(selector).localize(opts); 
+    selectorAttr: 'data-i18n', // selector for translating elements 
+    targetAttr: 'i18n-target', // data-() attribute to grab target element to translate (if diffrent then itself) 
+    optionsAttr: 'i18n-options', // data-() attribute that contains options, will load/set if useOptionsAttr = true 
+    useOptionsAttr: false, // see optionsAttr 
+    parseDefaultValueFromContent: true // parses default values from content ele.val or ele.text 
+});
